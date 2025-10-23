67c8a35356b3d64109d48b5f9e51677b6e08d739
thk7964 <yjs7894@gmail.com>
2025-10-23 16:20:28 +0900

    lv3 : main의 제어 기능을 분리해 kiosk 클래스가 담당하도록 구조 개선

--- src/kiosk/Kiosk.java
+++ src/kiosk/Kiosk.java
@@ -1,4 +1,46 @@
 package kiosk;
 
+import java.util.*;
+
 public class Kiosk {
+    //속성
+    private final List<MenuItem> menuItems;// List 선언 및 초기화
+    private boolean flag = true;//프로그램 반복 실행 여부를 제어하는 변수
+    private final Scanner sc = new Scanner(System.in);// Scanner 선언
+
+    //생성자
+    public Kiosk(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    //기능
+    public void start() {// 키오스크 실행
+        System.out.println("[ isaac toast MENU ]");
+        for (int i = 0; i < this.menuItems.size(); i++) {//List에 저장된 메뉴를 반복문으로 출력
+            MenuItem menuItem = this.menuItems.get(i);
+            System.out.println((i + 1) + ". " + menuItem.getName() + "\t| W " + menuItem.getPrice() + " | " + menuItem.getDescription());
+        }
+        System.out.println("0. 종료\t| 종료");
+
+        while (flag) {
+            try {
+                System.out.println("메뉴를 선택하세요");
+                int choice = sc.nextInt();// 메뉴 선택
+                //사용자가 입력한 번호에 따라 다른 동작 수행
+                if (choice >= 1 && choice <= this.menuItems.size()) {
+                    MenuItem selectMenu = this.menuItems.get(choice - 1);
+                    System.out.println("선택한 메뉴 :" + selectMenu.getName() + ", "
+                            + selectMenu.getPrice() + ", "
+                            + selectMenu.getDescription());
+                } else if (choice == 0) {
+                    flag = false;// 프로그램 종료
+                } else {
+                    System.out.println("잘못 입력했습니다.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("잘못 입력했습니다.");
+                sc.next();//잘못 입력된 값 비우기
+            }
+        }
+    }
 }
--- src/kiosk/Main.java
+++ src/kiosk/Main.java
@@ -6,8 +6,6 @@ public class Main {
     public static void main(String[] args) {
 
         List<MenuItem> menuItems = new ArrayList<>();// List 선언 및 초기화
-        boolean flag = true;//프로그램 반복 실행 여부를 제어하는 변수
-        Scanner sc = new Scanner(System.in);// Scanner 선언
 
         //각 메뉴를 List에 저장
         menuItems.add(new MenuItem("햄치즈 토스트", 3.3, "햄과 치즈의 완벽한 조합, 이삭토스트 시그니처 메뉴"));
@@ -19,33 +17,8 @@ public class Main {
         menuItems.add(new MenuItem("딥치즈 베이컨 포테이토", 5.3, "해시브라운과 베이컨, 딥치즈 소스의 깊은 풍미를 느껴보세요"));
         menuItems.add(new MenuItem("스크램블 베이컨", 4.8, "부드러운 스크램블과 짭짤한 베이컨, 치즈의 조화를 느껴보세요"));
 
-        System.out.println("[ isaac toast MENU ]");
-        for (int i = 0; i < menuItems.size(); i++) {//List에 저장된 메뉴를 반복문으로 출력
-            MenuItem menuItem = menuItems.get(i);
-            System.out.println((i + 1) + ". " + menuItem.getName() + "\t| W " + menuItem.getPrice() + " | " + menuItem.getDescription());
-        }
-        System.out.println("0. 종료\t| 종료");
-
-        while (flag) {
-            try {
-                System.out.println("메뉴를 선택하세요");
-                int choice = sc.nextInt();// 메뉴 선택
-                //사용자가 입력한 번호에 따라 다른 동작 수행
-                if (choice >= 1 && choice <= menuItems.size()) {
-                    MenuItem selectMenu = menuItems.get(choice - 1);
-                    System.out.println("선택한 메뉴 :" + selectMenu.getName() + ", "
-                            + selectMenu.getPrice() + ", "
-                            + selectMenu.getDescription());
-                } else if (choice == 0) {
-                    flag = false;// 프로그램 종료
-                } else {
-                    System.out.println("잘못 입력했습니다.");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("잘못 입력했습니다.");
-                sc.next();//잘못 입력된 값 비우기
-            }
-        }
+        Kiosk kiosk = new Kiosk(menuItems);// menuItems에 저장된 메뉴값을 Kiosk 클래스에 보낸다.
+        kiosk.start();// 키오스크 실행
         System.out.println("프로그램을 종료합니다.");
     }
 }
