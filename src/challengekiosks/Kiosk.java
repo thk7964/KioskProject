a3b1368983af0d5143a200f99ec30edfe7e95858
thk7964 <yjs7894@gmail.com>
2025-10-27 16:25:31 +0900

    fix : 잘못 입력햇을 때 공백이 나와서 한번 더 입력을 해야 되는 오류가 발생해서 sc.next()로 초기화 해주는걸 sc.nextLine()으로 바꿈

--- src/challengekiosks/Kiosk.java
+++ src/challengekiosks/Kiosk.java
@@ -23,7 +23,9 @@ public class Kiosk {
 
             try {
                 System.out.println("카테고리를 선택하세요");
+
                 int categoryChoice = sc.nextInt();// 카테고리를 선택
+
                 //사용자가 입력한 번호에 따라 다른 동작 수행
                 if (categoryChoice >= 1 && categoryChoice <= (this.menuList.size())) {
                     Menu selectCategory = this.menuList.get(categoryChoice - 1);
@@ -32,49 +34,48 @@ public class Kiosk {
                     System.out.println("0. 되돌아가기");
                     System.out.println("======주문할 메뉴를 선택해주세요======");
                     orderMenu(selectCategory);
-                }else if (categoryChoice==4){
-                    if(isOrderChecked){
+                } else if (categoryChoice == 4) {
+                    if (isOrderChecked) {
                         System.out.println("주문한다!!!!");
-                    }
-                    else {
-                        System.out.println("잘못 입력했습니다.");
-                    }
-                } else if (categoryChoice==5) {
-                    if(isOrderChecked){
-                        System.out.println("주문내역 삭제한다!!");
-                        isOrderChecked=false;
-                    }
-                    else {
+                    } else {
                         System.out.println("잘못 입력했습니다.");
 
                     }
+                } else if (categoryChoice == 5) {
+                    if (isOrderChecked) {
+                        System.out.println("주문내역 삭제한다!!");
+                        isOrderChecked = false;
+                    } else {
+                        System.out.println("잘못 입력했습니다.");
+                    }
                 } else if (categoryChoice == 0) {
                     isCategory = false;// 프로그램 종료
-                }else{//범위가 넘어가면 오류 발생
+                } else {//범위가 넘어가면 오류 발생
                     throw new IllegalArgumentException("카테고리 번호가 잘못됨");
                 }
             } catch (InputMismatchException | IllegalArgumentException e) {
                 System.out.println("잘못 입력했습니다.");
-                sc.next();//잘못 입력된 값 비우기
+                sc.nextLine();//잘못 입력된 값 비우기
             }
         }
         System.out.println("프로그램을 종료합니다.");
     }
 
     //카테고리 메뉴 화면에 출력
-    public void showCategory(){
+    public void showCategory() {
         System.out.println("[ isaac toast MENU ]");
         for (int i = 0; i < this.menuList.size(); i++) {//List에 저장된 메뉴를 반복문으로 출력
             Menu categoryMenu = this.menuList.get(i);
             System.out.println((i + 1) + ". " + categoryMenu.getCategory());
         }
         System.out.println("0. 종료");
-        if(isOrderChecked){
+        if (isOrderChecked) {
             System.out.println("[ ORDER MENU ]");
             System.out.println("4. ORDER ");
             System.out.println("5. Cancel ");
         }
     }
+
     //사용자가 선택한 메뉴를 출력
     public void orderMenu(Menu selectCategory) {
         while (isMenu) {
@@ -90,24 +91,23 @@ public class Kiosk {
                             + selectMenu.getDescription());
                     System.out.println(" 위 메뉴를 장바구니에 추가 하시겠습니까?");
                     System.out.println("1. 확인\t 2. 취소");
-                    int cartInput= sc.nextInt();//장바구니에 추가
-                    if(cartInput==1){
-                        System.out.println(selectMenu.getName()+"이 장바구니에 추가되었습니다.");
-                        isOrderChecked=true;
-                    }else if (cartInput==2){
+                    int cartInput = sc.nextInt();//장바구니에 추가
+                    if (cartInput == 1) {
+                        System.out.println(selectMenu.getName() + "이 장바구니에 추가되었습니다.");
+                        isOrderChecked = true;
+                    } else if (cartInput == 2) {
                         System.out.println("취소 되었습니다.");
-                    }else{//범위가 넘어가면 오류 발생
+                    } else {//범위가 넘어가면 오류 발생
                         throw new IllegalArgumentException("주문 확인 번호가 잘못됨");
-
                     }
                 } else if (choice == 0) {
                     isMenu = false;// 카테고리 선택 창으로 되돌아가기
-                } else{//범위가 넘어가면 오류 발생
+                } else {//범위가 넘어가면 오류 발생
                     throw new IllegalArgumentException("메뉴 번호가 잘못됨");
                 }
             } catch (InputMismatchException | IllegalArgumentException e) {
                 System.out.println("잘못 입력했습니다.");
-                sc.next();//잘못 입력된 값 비우기
+                sc.nextLine();//잘못 입력된 값 비우기
             }
         }
     }
