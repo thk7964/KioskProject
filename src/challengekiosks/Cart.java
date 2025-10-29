1960b05a9c959e573268ce92210c62630fab0281
thk7964 <yjs7894@gmail.com>
2025-10-29 12:55:17 +0900

    refactor : 메뉴 수량 체크 기능을 Cart 클래스에서 CartItem 클래스로 이동

--- src/challengekiosks/Cart.java
+++ src/challengekiosks/Cart.java
@@ -5,31 +5,22 @@ import java.util.*;
 public class Cart {
     //속성
     private final List<CartItem> orderList = new ArrayList<>();
-
     //생산자
 
     //기능
     //장바구니 안의 메뉴를 보여준다.
     public void showOrderList() {
         System.out.println("[ Order Menu ]");
-        //이미 처리한 메뉴 이름을  저장해 중복을 방지
-        List<String> pickName = new ArrayList<>();
         //장바구니 목록 확인
         for (CartItem item : orderList) {
-            int cnt = 0;//메뉴 주문 수량
+
+            //메뉴 주문 수량
             String name = item.getMenuItem().getName();
             double price = item.getMenuItem().getPrice();
-            if (pickName.contains(name)) continue;//처리한 중복 메뉴가 있을시 다시 출력을 하지 않도록 한다.
-            for (CartItem cartItem : orderList) {// 같은 이름의 메뉴가 몇 번 담겼는지 계산
-                if (cartItem.getMenuItem().getName().equals(name)) {
-                    cnt++;
-                }
-            }
+            int quantity = item.getQuantity();
             // 메뉴 정보 및 계산 결과 출력
             System.out.printf("%s | w %.2f | %s | 주문 수량 :  %d 개 | 금액 합 : %.2f%n",
-                    name, price, item.getMenuItem().getDescription(), cnt, (price * cnt));
-            // 처리한 메뉴 저장
-            pickName.add(name);
+                    name, price, item.getMenuItem().getDescription(), quantity, (price * quantity));
         }
         //장바구니 전체 금액 합계 출력
         System.out.println("[ Total ]");
@@ -38,6 +29,14 @@ public class Cart {
 
     //메뉴를 장바구니에 저장한다.
     public void addCart(MenuItem item) {
+        for (CartItem cartItem : orderList) {
+            //중복된 메뉴가 있으면 수량만 증가 후 종료
+            if (cartItem.getMenuItem().getName().equals(item.getName())) {
+                cartItem.increaseQuantity();
+                return;
+            }
+        }
+        //장바구니에 중복된 메뉴가 없으면 메뉴 추가
         orderList.add(new CartItem(item));
     }
 
@@ -50,7 +49,8 @@ public class Cart {
     public double getTotalPrice() {
         double sum = 0;
         for (CartItem item : orderList) {
-            sum += item.getMenuItem().getPrice();
+            //메뉴의 금액과 수량을 곱한다.
+            sum += item.getMenuItem().getPrice() * item.getQuantity();
         }
         return sum;
     }
--- src/challengekiosks/CartItem.java
+++ src/challengekiosks/CartItem.java
@@ -3,13 +3,27 @@ package challengekiosks;
 public class CartItem {
     //속성
     private final MenuItem menuItem;//장바구니에 담긴 메뉴 아이템
+    private int quantity;
+
     //생산자
-    public CartItem(MenuItem menuItem){
-        this.menuItem=menuItem;
+    public CartItem(MenuItem menuItem) {
+        this.menuItem = menuItem;
+        quantity = 1;// 초기 수량은 1
     }
+
     //기능
     //메뉴 아이템 반환(게터)
-    public MenuItem getMenuItem(){
+    public MenuItem getMenuItem() {
         return menuItem;
+    }
+
+    //장바구니에 담긴 메뉴의 수량 반환
+    public int getQuantity() {
+        return quantity;
+    }
+
+    //이미 장바구니에 담긴 메뉴면 수량 증가
+    public void increaseQuantity() {
+        quantity++;
     }
 }
